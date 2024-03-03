aa7c076d87a079ec2bf6f57e24c498fb480732fe
PRAVEEN200307 <crouselearn@gmail.com>
2024-03-02 22:14:10 -0500

    fix:sign up  page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         signup: resolve(__dirname, "signup.html"),
-        signup: resolve(__dirname, "login.html"),
+        login: resolve(__dirname, "login.html"),
       },
     },
   },
